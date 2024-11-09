89aa77d66e3864b615d1bc003407a2db9543c598
James Campbell <dr.j.campbell@gmail.com>
2024-11-09 00:18:29 +0000

    look and feel plus switch from index to name

--- app/moldle.py
+++ app/moldle.py
@@ -23,66 +23,54 @@ def read_compounds(file = "../resources/compounds.tsv", max_name_length = 100):
     return compounds
 
 # pick a compound at random from the dataset
-def choose_compound_index(compounds):
+def choose_compound(compounds) -> None:
     compound_index = random.randint(0, (len(compounds["name"]) - 1))
-    
-    return compound_index
+    st.session_state.compound_name = st.session_state.compounds['name'][compound_index]
+    st.session_state.compound_smiles = st.session_state.compounds['SMILES'][compound_index]
 
 # show the compound using rdkit
-def draw_smiles(compounds, compound_index, png_file = "compound.png") -> None:
-    m = Chem.MolFromSmiles(st.session_state.compounds["SMILES"][st.session_state.compound_index])
-    img = Draw.MolToImage(m)
+def draw_smiles(compounds, compound_name, png_file = "compound.png") -> None:
+    m = Chem.MolFromSmiles(st.session_state.compound_smiles)
+    img = Draw.MolToImage(m, size=(300, 150))
     img.save(png_file)
     st.image(png_file)
 
 # find plausible similar names (red herrings)
 # break name into parts, compare other compound name parts
 # score based on count of common parts
-def get_red_herrings(compounds, compound_index):
-    red_herrings = set()
-    while len(red_herrings) < 4:
-        j = random.randint(0, (len(compounds["SMILES"]) - 1))
-        if j == compound_index:
+def get_red_herrings(compounds, compound_name):
+    red_herrings = set([st.session_state.compound_name])
+    while len(red_herrings) < 5:
+        rh_index = random.randint(0, (len(compounds['name']) - 1))
+        rh_name = st.session_state.compounds['name'][rh_index]
+        if rh_name == st.session_state.compound_name:
             continue
-        red_herrings.add(j)
+        red_herrings.add(rh_name)
     red_herrings = list(red_herrings)
-    red_herrings.append(compound_index)
     red_herrings.sort()
     return red_herrings
 
 def check_user_guess() -> None:
-    if st.session_state.guess == st.session_state.compounds["name"][st.session_state.compound_index]:
+    if st.session_state.guess == st.session_state.compound_name:
         st.session_state.user_score += 1
     else:
         st.session_state.wrong = True
-        st.session_state.should_have_been = st.session_state.compounds["name"][st.session_state.compound_index]
+        st.session_state.should_have_been = st.session_state.compound_name
         st.session_state.user_score = 0
-    st.session_state.compound_index = choose_compound_index(st.session_state.compounds)
-    st.session_state.red_herrings = get_red_herrings(st.session_state.compounds, st.session_state.compound_index)
+    choose_compound(st.session_state.compounds)
+    st.session_state.red_herrings = get_red_herrings(st.session_state.compounds, st.session_state.compound_name)
     
 
 def user_guess(compounds, red_herrings, compound_index):
     st.session_state.guess = st.radio(
-        "**Select a compound**",
-        [
-            compounds["name"][red_herrings[0]],
-            compounds["name"][red_herrings[1]],
-            compounds["name"][red_herrings[2]],
-            compounds["name"][red_herrings[3]],
-            compounds["name"][red_herrings[4]]
-        ],
+        "**Choose a name:**",
+        st.session_state.red_herrings,
         index=None
     )
     check_guess = st.button(
         "Submit",
         on_click = check_user_guess
         )
-
-# def user_guess(compounds, red_herrings, compound_index):
-#     st.write(red_herrings)
-#     st.write(compound_index)
-
-
 
 def main():
     if "wrong" not in st.session_state:
@@ -92,13 +80,14 @@ def main():
         st.session_state.user_score = 0
     if "compounds" not in st.session_state:
         st.session_state.compounds = read_compounds()
-    if 'compound_index' not in st.session_state:
-        st.session_state.compound_index = choose_compound_index(st.session_state.compounds)
+    if 'compound_name' not in st.session_state:
+        choose_compound(st.session_state.compounds)
     if 'red_herrings' not in st.session_state:
-        st.session_state.red_herrings = get_red_herrings(st.session_state.compounds, st.session_state.compound_index)
+        st.session_state.red_herrings = get_red_herrings(st.session_state.compounds, st.session_state.compound_name)
     st.image('../resources/logo.png', width=300)
-    draw_smiles(st.session_state.compounds, st.session_state.compound_index, png_file = "compound.png")
-    user_guess(st.session_state.compounds, st.session_state.red_herrings, st.session_state.compound_index)
+    st.write('**Name this molecule:**')
+    draw_smiles(st.session_state.compounds, st.session_state.compound_name, png_file = "compound.png")
+    user_guess(st.session_state.compounds, st.session_state.red_herrings, st.session_state.compound_name)
     if st.session_state.wrong:
         st.write(f"The correct answer was {st.session_state.should_have_been}")
         st.session_state.wrong = False
